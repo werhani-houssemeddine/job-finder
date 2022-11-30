9296f227c3452fbb90dfee3d0961e8b99fce93c8
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2022-11-30 21:50:24 +0100

    I setup the files

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <div id="root">
+    <?php
+      if(file_exists('./ressources/app.php')){
+        require_once './ressources/app.php';
+      } else {
+        echo 'Warning Message !';
+        exit(1);
+      }
+    ?>
+  </div>
+</body>
+</html>
